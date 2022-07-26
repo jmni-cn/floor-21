e3c747f88de348b042bfcd4c2c0c1d9be3a4d2f2
BILIBILI\v_zhangjiaming <https://git.bilibili.co/>
2022-07-26 14:45:03 +0800

    打包

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ npm cache clear --force
 npm install 
 # npm run build
 npm run jenbuild
-mv /var/lib/jenkins/workspace/floor-21-admin/floor-21/  /var/www/webapp/
+# mv /var/lib/jenkins/workspace/floor-21-admin/floor-21/  /var/www/webapp/
